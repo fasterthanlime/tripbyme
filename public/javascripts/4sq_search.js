6fcca51d951e20705ee338798270320adbb24294
Amos Wenger <ndd@rylliog.cz>
2011-03-19 11:52:09 +0100

    dynamic search

--- public/javascripts/4sq_search.js
+++ public/javascripts/4sq_search.js
@@ -1,9 +1,12 @@
 
-function 4sq_search(latLng, map) {  
+function fsq_search(latLng, map) {  
   var client_id     = "HMEWANYJXE0U5UDKIYWYAQLFWVABI4TBGMR3MATVJEFKOHZ0"
   var client_secret = "5SQ0UEBC5THYYBDC3OBLGBW4LWPNF0SN3S4TT5JGHJJRNXQX"
-  var url = "https://api.foursquare.com/v2/venues/search?ll=#" + latLng.toUrlValue() + "&client_id=" + client_id + "&client_secret=" + client_secret
+  var url = "https://api.foursquare.com/v2/venues/search?ll=" + latLng.toUrlValue() + "&client_id=" + client_id + "&client_secret=" + client_secret
   
-  //$.ajax();
+  $.getJSON(url, function(data) {
+      var items = data["response"]["groups"][0]["items"];
+      $("#results_count").html(items.size() + " results");
+  });
 }
 
