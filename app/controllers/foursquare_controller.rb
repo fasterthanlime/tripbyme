e4547570ea13c094615311cf7e23983039d0aa9e
Amos Wenger <ndd@rylliog.cz>
2011-03-19 09:49:13 +0100

    Cleaner CSS, category icons

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -7,6 +7,7 @@ class FoursquareController < ApplicationController
     url = "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=HMEWANYJXE0U5UDKIYWYAQLFWVABI4TBGMR3MATVJEFKOHZ0&client_secret=5SQ0UEBC5THYYBDC3OBLGBW4LWPNF0SN3S4TT5JGHJJRNXQX"
     clnt = HTTPClient.new
     jsonObj = JSON.parse(clnt.get_content(url))
+    @count = 10
     @items = jsonObj["response"]["groups"][0]["items"]
     #@results = JSON.pretty_generate(jsonObj)
   end
