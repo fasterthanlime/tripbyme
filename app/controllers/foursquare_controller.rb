61838edd920b992ab34c686ce9176e5cdcefc62e
Amos Wenger <ndd@rylliog.cz>
2011-03-19 10:43:22 +0100

    Added logo, added polyline

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -4,8 +4,14 @@ require 'json'
 
 class FoursquareController < ApplicationController
   def search
-    @lat = 40.7
-    @lng = -74
+    ## brooklyn
+    #@lat = 40.7
+    #@lng = -74
+    
+    # lausanne
+    @lat = 46.529816
+    @lng = 6.630592
+    
     client_id     = "HMEWANYJXE0U5UDKIYWYAQLFWVABI4TBGMR3MATVJEFKOHZ0"
     client_secret = "5SQ0UEBC5THYYBDC3OBLGBW4LWPNF0SN3S4TT5JGHJJRNXQX"
     url = "https://api.foursquare.com/v2/venues/search?ll=#{@lat},#{@lng}&client_id=#{client_id}&client_secret=#{client_secret}"
