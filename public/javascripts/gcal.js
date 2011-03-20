ae76020c59c09a638406f4bf8d34eadae0467887
happydawn <crackofdusk@gmail.com>
2011-03-20 11:34:27 +0100

    google calander add butto javascript helper

---
+++ public/javascripts/gcal.js
@@ -0,0 +1,21 @@
+function generateCalendarURI(name, start_time, stop_time, lat, lng, description) {
+    sname = encodeURI(name);
+    sdates = encodeURI(start_time + "/" + stop_time);
+    sdescription = encodeURI(description);
+    slocation = encodeURI(lat + "," + lng);
+    swebsite = encodeURI("planaday.ch");
+    swebsitename = encodeURI("planaday");
+
+    uri = "http://www.google.com/calendar/event?action=TEMPLATE";
+    uri += "&text=" + sname;
+    uri += "&dates=" + sdates;
+    uri += "&location=" + slocation;
+    uri += "&trp=fals&sprop=" + swebsite + "&sprop=name:" + swebsitename;
+
+    return uri;
+}
+
+function generateCalendarButton(name, start_time, stop_time, lat, lng, description) {
+    return '<a href="' + generateCalendarURI() + '" target="_blank"><img src="http://www.google.com/calendar/images/ext/gc_button6.gif" border=0></a>'
+
+}
